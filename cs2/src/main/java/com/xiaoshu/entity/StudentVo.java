00dfdbb7a1a0c5c016dc05d750a98c93f9e4a061
wth <2565833317@qq.com>
2020-07-20 16:07:19 +0800

    周末作业2

---
+++ cs2/src/main/java/com/xiaoshu/entity/StudentVo.java
@@ -0,0 +1,15 @@
+package com.xiaoshu.entity;
+
+public class StudentVo extends Student{
+	private String sname;
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+
+}
